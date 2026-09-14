474ca615b0e2d3edf5189fd9185e316d961b9088
cxjchelsea <147217475+cxjchelsea@users.noreply.github.com>
2026-09-14 11:46:16 +0800

    feat: extend capability trace schema

---
+++ diagnosis-service/src/main/resources/db/migration/V5__extend_capability_trace.sql
@@ -0,0 +1,2 @@
+ALTER TABLE clinical_capability_call_trace ADD COLUMN rule_release_ref VARCHAR(128) NULL;
+ALTER TABLE clinical_capability_call_trace ADD COLUMN knowledge_release_ref VARCHAR(128) NULL;
